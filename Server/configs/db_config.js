ecdf3c2a18fc0a9f8a9ff2b0b60c46bea2edef06
Fhatuwani Raphalalani <fm.raphalalani29@gmail.com>
2023-05-12 19:53:42 +0200

    initial commit

--- Server/configs/db_config.js
+++
@@ -1,8 +0,0 @@
-
-const configData = {
-    conn_string: 'mongodb+srv://admin_fhatu:Password123@webmobileapplication.jx4opz3.mongodb.net/MongooseAPI?retryWrites=true&w=majority',
-    PORT: 8001,
-    Token_secret: 'fhatweferasefswee12341'
-}
-
-module.exports = configData;
