57d32724565b56724435ade6b26c58d4c1ad9b6d
BigguyFM98000 <fm.raphalalani29@gmail.com>
2023-06-11 01:36:59 +0200

    edited controller routes

--- Server/routes/user_routes.js
+++ Server/routes/user_routes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const UserController = require('../controllers/user_controller')
 const router = express.Router();
 
+router.get('/status', (req, res) => {
+    res.json({message: "User api is successfully live!!!!" });
+});
 router.get('/:id', UserController.findOne);
 router.put('/:id', UserController.update);
 router.delete('/:id', UserController.destroy);
