9ecfba7b26a0f8506399dbee96628cd2615eda66
BigguyFM98000 <fm.raphalalani29@gmail.com>
2023-06-11 01:06:28 +0200

    initial commit

--- Server/routes/reset_password.js
+++ Server/routes/reset_password.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ForgotPasswordController = require('../controllers/forgot_password');
 const router = express.Router();
 
-router.get("/", () => {
+router.get("/status", (req, res) => {
     res.json({message: "Welcome to Reset API" });
 });
 router.post("/forgotpassword", ForgotPasswordController.send);
