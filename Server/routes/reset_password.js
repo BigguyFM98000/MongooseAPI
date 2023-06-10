14d9c11e4f5bc1aa200080ca849c1597c70193e3
BigguyFM98000 <fm.raphalalani29@gmail.com>
2023-06-11 00:20:44 +0200

    updated reset routes

--- Server/routes/reset_password.js
+++ Server/routes/reset_password.js
@@ -2,8 +2,8 @@ const express = require('express')
 const ForgotPasswordController = require('../controllers/forgot_password');
 const router = express.Router();
 
-router.post("/forgot-password", ForgotPasswordController.send);
-router.get("reset-password/:token", ForgotPasswordController.redirect);
-router.post("/reset-password/:token", ForgotPasswordController.resetform);
+router.post("/forgotpassword", ForgotPasswordController.send);
+router.get("/form/:token", ForgotPasswordController.redirect);
+router.post("/password/:token", ForgotPasswordController.resetform);
 
 module.exports = router;
