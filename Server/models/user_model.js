c9c189cce1f1602ac4600982e4d320b8a2ba1588
BigguyFM98000 <fm.raphalalani29@gmail.com>
2023-07-01 06:19:51 +0200

    date fields added

--- Server/models/user_model.js
+++ Server/models/user_model.js
@@ -2,47 +2,56 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    department: {
-        type: String,
-        default: 'Talent Acquisition Services'
-    },
-    profileImage: { type: String, default: 'https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png' },
-    source: {
-        type: String,
-        default: "normal-auth",
-      },
-      resetToken: {
-        type: String,
-      },
-      resetTokenExpiration: {
-        type: String,
-      },
-    lastVisited: {
-        type: Date,
-        default: new Date(),
-      },
-    dateCreated: {
-        type: Date,
-        default: () => Date.now(),
-    },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  department: {
+    type: String,
+    default: 'Talent Acquisition Services'
+  },
+  profileImage: {
+    type: String, default: 'https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png'
+  },
+  source: {
+    type: Date,
+    default: "normal-auth",
+  },
+  resetToken: {
+    type: Date,
+    default: "787a896be5383be3be12f63c6cc054e76b95f8c0ee556e2688bb756394773d84"
+  },
+  resetTokenSentTime: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetTokenExpirationTime: {
+    type: Date,
+    default: Date.now(),
+  },
+  lastVisited: {
+    type: Date,
+    default: new Date().toLocaleString()
+    ,
+  },
+  dateCreated: {
+    type: Date,
+    default: () => Date.now(),
+  },
 });
 
 const User = mongoose.model('User', userSchema);
